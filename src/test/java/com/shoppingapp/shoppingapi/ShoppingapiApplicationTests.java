af36c30d726c022fd704654a926d03d3f0202fba
Aaryan <aaryan14032006@gmail.com>
2021-09-02 17:36:36 +0530

    Runner

---
+++ src/test/java/com/shoppingapp/shoppingapi/ShoppingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingapp.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
