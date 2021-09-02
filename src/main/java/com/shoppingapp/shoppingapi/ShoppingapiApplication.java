0ee9f3d6c3991861fda3039d465d1c9b018455b3
Aaryan <aaryan14032006@gmail.com>
2021-09-02 17:35:26 +0530

    Runner

---
+++ src/main/java/com/shoppingapp/shoppingapi/ShoppingapiApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingapp.shoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingapiApplication.class, args);
+	}
+
+}
